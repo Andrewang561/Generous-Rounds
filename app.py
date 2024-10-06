eb4065dc74e0a9bb4004c6a19680657c315dfc13
andrewwang <andrewang561@hotmail.com>
2024-10-05 19:08:18 -0700

    Adjusted the files to support flask configuration.

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect
+from pymongo import MongoClient
+from wrkzeug.security import generate_password_hash
+
+app = Flask(__name__)
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client.charities.users
+users_collection = db.users
+
+@app.route('/')
+def index():
+    return render_template('sign.html')
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    name = request.form['name']
+    email = request.form['email']
+    password = request.form['password']
+
+    hashed_password = generate_password_hash(password, method='sha256')
+
+    user = {
+        "name": name,
+        "email": email,
+        "password": hashed_password
+    }
+
+    try:
+        users_collection.insert_one(user)
+        return "Registered successfully!"
+    except:
+        return "An error has occurred. Please try again."
+
+if __name__ == '__main__':
+    app.run(debug=True)
